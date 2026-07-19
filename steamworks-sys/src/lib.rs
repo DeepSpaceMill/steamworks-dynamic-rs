cda514c03689f03f5de7107b74ca7b9ddbe88211
Icemic <bingfeng.web@gmail.com>
2026-07-19 22:39:47 +0800

    feat(sys): always run bindgen on compiling

--- steamworks-sys/src/lib.rs
+++ steamworks-sys/src/lib.rs
@@ -2,14 +2,4 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_snake_case)]
 
-#[cfg(target_os = "windows")]
-include!("windows_bindings.rs");
-
-#[cfg(target_os = "macos")]
-include!("macos_bindings.rs");
-
-#[cfg(all(target_os = "linux", not(target_arch = "aarch64")))]
-include!("linux_bindings.rs");
-
-#[cfg(all(target_os = "linux", target_arch = "aarch64"))]
-include!("linuxarm_bindings.rs");
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
